846345bdfe677bc07c9e7898194f5c3c45696077
Kivashin Naidoo <kivashin47@gmail.com>
2024-10-27 19:44:03 +0200

    add jfx stuff

---
+++ Practical 10/app/src/main/java/practical/gui.java
@@ -0,0 +1,43 @@
+// package practical;
+
+// import javafx.fxml.FXML;
+// import javafx.scene.control.Button;
+// import javafx.scene.control.Label;
+// import javafx.scene.control.ListView;
+// import javafx.scene.control.TableColumn;
+// import javafx.scene.control.TableView;
+// import javafx.scene.input.MouseEvent;
+
+// public class gui {
+
+//     @FXML
+//     private TableColumn<String, Integer> actorName;
+
+//     @FXML
+//     private Label actorNum;
+
+//     @FXML
+//     private TableView<?> actorPerMovie;
+
+//     @FXML
+//     private TableColumn<?, ?> numMovies;
+
+//     @FXML
+//     private Button queryButton;
+
+//     @FXML
+//     private ListView<?> sameMovie;
+
+//     @FXML
+//     void connectDB(MouseEvent event) {
+
+//     }
+
+//     @FXML
+//     void runQueries(MouseEvent event) {
+
+//     }
+
+// }
+
+
